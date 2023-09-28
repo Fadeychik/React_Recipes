90460b40a9b16a4236fe3270d8b258ee76b2c512
Fadeychik <fadeeff.sereja2013@yandex.ru>
2023-09-28 18:22:33 +0300

    added gh-pages

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ function Footer() {
     <div className="footer-copyright">
       <div className="container">
       © {new Date().getFullYear()} Copyright Text
-      <a className="grey-text text-lighten-4 right" href="https://github.com/Fadeychik/Fortnite_Shop" target='_blank' rel='noreferrer'>More Links</a>
+      <a className="grey-text text-lighten-4 right" href="https://Fadeychik.github.io/React_Recipes" target='_blank' rel='noreferrer'>More Links</a>
       </div>
     </div>
   </footer>
